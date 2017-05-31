39f6ac5a941be783d04719e34bfea309178be91b
Jose Monge <jmonge@headlesschickengames.com>
2017-05-30 19:08:25 -0600

    Commit Inicial
    Esta es la version inicial del repositorio

---
+++ Assets/CharacterBehavior.cs
@@ -0,0 +1,79 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CharacterBehavior : MonoBehaviour {
+
+public float health = 100f; 
+public Text healthUI;
+public float damagePerTime = 1f;
+public float timeToDamage = 1f;
+
+public int forceToApplyJump;
+public int forceToApply;
+private float totalTime;
+
+private Vector3 beginPosition;
+
+	// Use this for initialization
+	void Start () {
+		Debug.Log("Hola!");
+		totalTime = 0;
+		beginPosition = transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		healthUI.text = "Health:"+ health;
+		
+
+		if(totalTime >= timeToDamage){
+			totalTime = 0;
+			health = health - damagePerTime;
+		}else{
+			totalTime += Time.deltaTime;
+		}
+
+		if(Input.GetKey(KeyCode.D)){
+			this.GetComponent<Rigidbody2D>().AddForce(Vector2.right*forceToApply,ForceMode2D.Force);
+		}
+
+		if(Input.GetKey(KeyCode.A)){
+			this.GetComponent<Rigidbody2D>().AddForce(Vector2.right*-forceToApply,ForceMode2D.Force);
+		}
+
+		if(Input.GetKey(KeyCode.Space)){
+			this.GetComponent<Rigidbody2D>().AddForce(Vector2.up*forceToApplyJump,ForceMode2D.Force);
+		}
+	}
+
+	/// <summary>
+	/// LateUpdate is called every frame, if the Behaviour is enabled.
+	/// It is called after all Update functions have been called.
+	/// </summary>
+	void LateUpdate()
+	{
+		
+	}
+
+	 /// <summary>
+	/// This function is called every fixed framerate frame, if the MonoBehaviour is enabled.
+	/// </summary>
+	void FixedUpdate()
+	{
+		
+	}
+
+	void Awake () {
+		Debug.Log("Adios");
+
+	}
+
+	public void resetPlayer()
+	{
+		transform.position = beginPosition;
+
+	}
+
+}
---
+++ Assets/FollowCamera.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowCamera : MonoBehaviour {
+
+	public Transform followTarget;
+	public float velocidadCam;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+		Vector2 temp = Vector2.MoveTowards(new Vector2(transform.position.x,transform.position.y),new Vector2(followTarget.position.x,followTarget.position.y),velocidadCam);
+		Vector3 nuevaPosicion = new Vector3(temp.x,temp.y,transform.position.z);
+		transform.position = nuevaPosicion;
+
+	}
+}
